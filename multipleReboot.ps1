22205e47fd6a039ca41a1c38f5c72f9d2a4d00b2
Shawn <folmar.shawn0@gmail.com>
2019-09-25 16:38:59 -0500

    Add files via upload
    This script will seach for a specific log event E.G. the event log service stopping, and find n number of the most recent instances, if any of the logs are within 5 minutes of each other it will display the time and properties of that event

---
+++ multipleReboot.ps1
@@ -0,0 +1,29 @@
+﻿
+
+$a =  @(get-eventlog system  | where-object {$_.eventid -eq 6006} | Select-Object -Property Source, EventID, InstanceId, Message, TimeGenerated)
+
+
+
+$compareDateOne = Get-Date ($a[0].TimeGenerated) 
+$compareDateTwo = Get-Date ($a[1].TimeGenerated) 
+$diff = NEW-TIMESPAN –Start $compareDateOne –End $compareDateTwo 
+
+for($i=0; $i -lt 5; $i++){
+
+    $n = $i + 1
+    $compareDateOne = Get-Date ($a[$i].TimeGenerated) 
+    $compareDateTwo = Get-Date ($a[$n].TimeGenerated) 
+    $diff = NEW-TIMESPAN –Start $compareDateOne –End $compareDateTwo 
+
+    if($diff.TotalMinutes -lt -420 ){
+    
+        Write-Output "its a boy"
+        Write-Output $a[$i] | Select -property *
+        Write-Output $diff.TotalMinutes
+
+    }else{
+
+        Write-Output "its not good bud"
+        Write-Output $a[$i] | Select -property *
+}
+}
